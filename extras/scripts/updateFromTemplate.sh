5c54e8ab24a1f0d17072fa7b5ab2a63ba1f7c0cd
joaquimrcarvalho <joaquim@uc.pt>
2019-05-28 17:20:47 +0100

    Added script for updating from the template.

---
+++ extras/scripts/updateFromTemplate.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# This updates a repository forked from this one with new stuff added here
+# after the fork.
+#
+# Use case: after this template repository is forked for a new community,
+# and then cloned to a workstation, the commands pull and push refer to the
+# forked repository, not this original one. If this template is updated after
+# the fork it is necessary to add it as a remote of the community fork and
+# pull.
+#
+# Use on a repository cloned from a fork of the timelink-sources-template repo.
+
+
+# Add the original repository and a remote.
+git remote add template https://timelink-sources.visualstudio.com/timelink-sources-template/_git/timelink-sources-template
+
+# Pull new stuff into current branch
+git pull template/master
+
+# remove the remote to prevent pushing by accident.
+git remote rm template
